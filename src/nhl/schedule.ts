79c23702be31b39d66a762da5778bdf65bfc79c3
RaphyLi <raphael.lichan@outlook.com>
2021-01-14 14:30:03 -0500

    feat(Schedule): Change wording message

--- src/nhl/schedule.ts
+++ src/nhl/schedule.ts
@@ -3,6 +3,7 @@ import qs from 'qs';
 import fetch from '../utils/fetch';
 import { getToday } from '../utils/helpers';
 import { NHL } from './models/nhl';
+import { Away, Home } from './models/teams';
 
 const BASE_URL = 'https://statsapi.web.nhl.com/api/v1';
 
@@ -21,7 +22,29 @@ export class Schedule {
                 const title = `${date ? date : getToday()}`;
                 const games = [];
                 result.dates.forEach(value => {
-                    value.games.forEach(game => games.push(`${game.teams.home.team.name} VS ${game.teams.away.team.name}`));
+                    value.games.forEach(game => {
+                        const home = game.teams.home;
+                        const away = game.teams.away;
+                        let message: string = '';
+                        if (date) {
+                            let winTeam: Home | Away;
+                            let losseTeam: Home | Away;
+
+                            if (home.leagueRecord.wins === 1) {
+                                winTeam = home;
+                                losseTeam = away;
+                            } else {
+                                losseTeam = home;
+                                winTeam = away;
+                            }
+
+                            message = `${winTeam.team.name} (${winTeam.score}) - ${losseTeam.team.name} (${losseTeam.score}) FINAL${losseTeam.leagueRecord.ot ? '/OT' : ''}`;
+                        } else {
+                            message = `${game.teams.home.team.name} VS ${game.teams.away.team.name}`;
+                        }
+
+                        games.push(message);
+                    });
                 });
                 resolve({
                     attachments: [
